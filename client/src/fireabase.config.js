0b510eb8a223b4a20bdae793160aa6d501d99ba8
Giovana Joice <giovanajoice11@gmail.com>
2023-05-16 20:18:29 -0300

    .

---
+++ client/src/fireabase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIC4W1f8R89RwRRAjiCFAKqy__v3bLOuo",
+  authDomain: "bazarapp-2afeb.firebaseapp.com",
+  projectId: "bazarapp-2afeb",
+  storageBucket: "bazarapp-2afeb.appspot.com",
+  messagingSenderId: "125969906337",
+  appId: "1:125969906337:web:d64e5ac9a9331b50508ee0"
+};
+
+// Initialize Firebase
+ export const app = initializeApp(firebaseConfig);
